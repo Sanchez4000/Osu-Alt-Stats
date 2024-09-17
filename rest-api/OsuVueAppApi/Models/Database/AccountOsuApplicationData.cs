0c83fd97feef996b282d235c6b6331caca5f809b
Alexandr Balabanov <Sanchez4000@mail.ru>
2024-09-17 14:34:29 +0300

    Создание модели OsuClient и добавление ее в контекст базы данных

---
+++ rest-api/OsuVueAppApi/Models/Database/AccountOsuApplicationData.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OsuVueAppApi.Models.Database
+{
+    [PrimaryKey(nameof(Id))]
+    public class OsuClient
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public string ClientSecret { get; set; } = string.Empty;
+    }
+}
