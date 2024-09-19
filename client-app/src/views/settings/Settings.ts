e129b0441d5a2010cbfc59a3039992b93071d5d6
Alexandr Balabanov <Sanchez4000@mail.ru>
2024-09-19 16:01:22 +0300

    Декомпозиция компонента настроек

--- client-app/src/views/settings/Settings.ts
+++ client-app/src/views/settings/Settings.ts
@@ -1,36 +1,11 @@
-import SettingsApi from "@/api/SettingsApi";
-import SettingsModel from "@/models/SettingsModel";
+import OsuClientSettings from "./components/osuClient/OsuClientSettings.vue";
 import { Component, Vue, toNative } from "vue-facing-decorator";
 
-@Component
-class Settings extends Vue {
-  private clientId = 0;
-  private clientSecret = "";
-  private canSeeSecret = false;
-
-  public get secretFieldType(): string {
-    return this.canSeeSecret ? "text" : "password";
-  }
-
-  private mounted(): void {
-    SettingsApi.loadSettings().then((r) => {
-      if (r.osuClient === null) return;
-
-      this.clientId = r.osuClient.clientId;
-      this.clientSecret = r.osuClient.clientSecret;
-    });
-  }
-
-  private save() {
-    const data: SettingsModel = {
-      osuClient: {
-        clientId: this.clientId,
-        clientSecret: this.clientSecret,
-      },
-    };
-
-    SettingsApi.saveSettings(data);
-  }
-}
+@Component({
+  components: {
+    OsuClientSettings,
+  },
+})
+class Settings extends Vue {}
 
 export default toNative(Settings);
--- client-app/src/views/settings/Settings.vue
+++ client-app/src/views/settings/Settings.vue
@@ -1,27 +1,6 @@
 <template>
   <div>
-    <div>
-      <label for="client-id">ClientId: </label>
-      <input type="number" name="ClientId" id="client-id" v-model="clientId" />
-    </div>
-    <div>
-      <label for="client-secret">ClientSecret: </label>
-      <input
-        :type="secretFieldType"
-        name="ClientSecret"
-        id="client-secret"
-        v-model="clientSecret"
-      />
-      <input
-        type="checkbox"
-        name="CanSeeSecret"
-        id="can-see-secret"
-        v-model="canSeeSecret"
-      />
-    </div>
-    <div>
-      <button type="submit" @click="save()">Сохранить</button>
-    </div>
+    <OsuClientSettings />
   </div>
 </template>
 <script src="./Settings.ts" />
