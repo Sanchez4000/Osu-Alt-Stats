d40556b81a5c93a4a596c25201aec390bf5d99bb
Alexandr Balabanov <Sanchez4000@mail.ru>
2024-09-19 17:47:54 +0300

    Создан компонент поля для настроек

---
+++ client-app/src/views/settings/ui/field/Field.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="field">
+    <div class="name">
+      <span>{{ name }}</span>
+    </div>
+    <div class="edit-tool">
+      <div><slot></slot></div>
+    </div>
+  </div>
+</template>
+<script src="./Field.ts" />
+<style src="./Field.css" scoped />
