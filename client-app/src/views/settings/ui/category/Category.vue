d64024f41b03460d6efd77a33ea58e6db0ec4b2a
Alexandr Balabanov <Sanchez4000@mail.ru>
2024-09-19 17:24:07 +0300

    Создан блок категории для настроек

---
+++ client-app/src/views/settings/ui/category/Category.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="category">
+    <h1 class="category-name">{{ name }}</h1>
+    <div class="category-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script src="./Category.ts" />
+<style src="./Category.css" scoped />
