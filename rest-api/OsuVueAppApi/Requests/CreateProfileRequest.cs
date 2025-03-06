236db0cd8764af3093137ebab65649a8f6d5252c
Alexandr Balabanov <Sanchez4000@mail.ru>
2025-03-06 20:48:44 +0300

    feat: Функция создания профиля

---
+++ rest-api/OsuVueAppApi/Requests/CreateProfileRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OsuVueAppApi.Requests
+{
+    public class CreateProfileRequest
+    {
+        [Required]
+        public required string Name { get; set; }
+    }
+}
