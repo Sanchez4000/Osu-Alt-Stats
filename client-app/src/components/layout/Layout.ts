b937e2e7acc88af882d89c743a64959baa915f6d
Alexandr Balabanov <Sanchez4000@mail.ru>
2025-03-06 18:16:01 +0300

    feat: Меню профилей на Layout

--- client-app/src/components/layout/Layout.ts
+++ client-app/src/components/layout/Layout.ts
@@ -1,21 +1,22 @@
 import { Component, Vue, toNative } from "vue-facing-decorator";
 import NavBarButton from "@/ui-kit/nav-bar-button/NavBarButton.vue";
+import ProfileBlock from "../profileBlock/ProfileBlock.vue";
 import AuthorizationApi from "@/api/AuthorizationApi";
 
 @Component({
   components: {
     NavBarButton,
+    ProfileBlock,
   },
 })
 class Layout extends Vue {
   private authLink = "";
 
   private mounted(): void {
-    AuthorizationApi.getAuthLink().then((r) => {
-      if (r === null) return;
-
-      this.authLink = r;
-    });
+    // AuthorizationApi.getAuthLink().then((r) => {
+    //   if (r === null) return;
+    //   this.authLink = r;
+    // });
   }
 }
 
