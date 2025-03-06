6f0778e0b01f04e8f103bf44cf0c491902fee94c
Alexandr Balabanov <Sanchez4000@mail.ru>
2025-03-06 20:29:50 +0300

    feat: Расширение модели пользователяу

--- rest-api/Common/DbModels/ProfileModel.cs
+++ rest-api/Common/DbModels/ProfileModel.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; } = 0;
         public required string Name { get; set; }
         public required bool IsActive { get; set; }
+        public string ApplicationId { get; set; } = string.Empty;
+        public string ApplicationSecret { get; set; } = string.Empty;
     }
 }
