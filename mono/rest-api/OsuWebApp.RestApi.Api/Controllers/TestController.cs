7168b54550db27a69268fd037116108e4cfde706
Alexandr Balabanov <Sanchez4000@mail.ru>
2025-06-30 14:18:52 +0300

    chore: Add initial ASP.NET Core Web API project
    Created a new ASP.NET Core Web API project with a TestController, basic configuration files, Swagger support, and solution setup. This provides the foundation for developing REST API endpoints.

---
+++ mono/rest-api/OsuWebApp.RestApi.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OsuWebApp.RestApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Test successful!");
+        }
+    }
+}
